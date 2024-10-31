24497f444ed51c1db74862dd030f9e2161aa9203
Jacob Menlove <jacobmenlove1@gmail.com>
2024-10-31 15:25:34 -0400

    readme added

--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -1,12 +1,14 @@
 describe('Tech Quiz E2E Test', () => {
     it('starts the quiz and completes all questions', () => {
-      cy.visit('/'); // Adjust URL as necessary for the app's root path
+      cy.visit('http://127.0.0.1:3001/'); // Adjust URL as necessary for the app's root path
       cy.get('button').contains('Start Quiz').click();
   
       // Loop through all questions
       for (let i = 0; i < 10; i++) {
         cy.get('.question').should('be.visible');
         cy.get('.answer-button').first().click(); // Click first answer for each
+        cy.get('.question', { timeout: 100000 }).should('be.visible');
+
       }
   
       // Check final score
